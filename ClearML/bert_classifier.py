355cd3b41b2d0246eb8b03ab13a895b1685be6be
whopriyam <priyambasu16@gmail.com>
2022-03-09 11:31:00 +0530

    ClearML and Guild old codes

---
+++ ClearML/bert_classifier.py
@@ -0,0 +1,224 @@
+# Importing the libraries needed
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+import torch
+import seaborn as sns
+import transformers
+import json
+from tqdm import tqdm
+from torch.utils.data import Dataset, DataLoader
+from transformers import RobertaModel, RobertaTokenizer
+import logging
+#logging.basicConfig(level=logging.ERROR)
+
+from clearml import Task
+Task.init(project_name='Trial', task_name='bert_class2')
+Task.set_offline(offline_mode=False)
+
+# Setting up GPU
+
+from torch import cuda
+device = 'cuda' if cuda.is_available() else 'cpu'
+print ("-"*50,cuda.is_available(),"-"*50)
+print (torch.version.cuda)
+#device = 'cuda'
+print ("-"*50,device,"-"*50)
+
+df = pd.read_csv('Data/ACL_FINAL_Data.csv')
+
+df.rename(columns = {'Text':'tweet', 'Label':'target'}, inplace = True)
+df.target = df.target.astype(int)
+
+print (df.head())
+
+#Preparing Dataset and Dataloader
+
+# Defining some key variables that will be used later on in the training
+MAX_LEN = 256
+TRAIN_BATCH_SIZE = 8
+VALID_BATCH_SIZE = 4
+# EPOCHS = 1
+LEARNING_RATE = 1e-05
+tokenizer = RobertaTokenizer.from_pretrained('roberta-base', truncation=True, do_lower_case=True)
+
+class TweetData(Dataset):
+    def __init__(self, dataframe, tokenizer, max_len):
+        self.tokenizer = tokenizer
+        self.data = dataframe
+        self.text = dataframe.tweet
+        self.targets = self.data.target
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.text)
+
+    def __getitem__(self, index):
+        text = str(self.text[index])
+        text = " ".join(text.split())
+
+        inputs = self.tokenizer.encode_plus(
+            text,
+            None,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            pad_to_max_length=True,
+            return_token_type_ids=True
+        )
+        ids = inputs['input_ids']
+        mask = inputs['attention_mask']
+        token_type_ids = inputs["token_type_ids"]
+
+
+        return {
+            'ids': torch.tensor(ids, dtype=torch.long),
+            'mask': torch.tensor(mask, dtype=torch.long),
+            'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long),
+            'targets': torch.tensor(self.targets[index], dtype=torch.float)
+        }
+
+train_size = 0.8
+train_data=df.sample(frac=train_size,random_state=200)
+test_data=df.drop(train_data.index).reset_index(drop=True)
+train_data = train_data.reset_index(drop=True)
+
+
+print("FULL Dataset: {}".format(df.shape))
+print("TRAIN Dataset: {}".format(train_data.shape))
+print("TEST Dataset: {}".format(test_data.shape))
+
+training_set = TweetData(train_data, tokenizer, MAX_LEN)
+testing_set = TweetData(test_data, tokenizer, MAX_LEN)
+
+train_params = {'batch_size': TRAIN_BATCH_SIZE,
+                'shuffle': True,
+                'num_workers': 0
+                }
+
+test_params = {'batch_size': VALID_BATCH_SIZE,
+                'shuffle': True,
+                'num_workers': 0
+                }
+
+training_loader = DataLoader(training_set, **train_params)
+testing_loader = DataLoader(testing_set, **test_params)
+
+#Base Roberta model
+class RobertaClass(torch.nn.Module):
+    def __init__(self):
+        super(RobertaClass, self).__init__()
+        self.l1 = RobertaModel.from_pretrained("roberta-base")
+        self.pre_classifier = torch.nn.Linear(768, 768)
+        self.dropout = torch.nn.Dropout(0.3)
+        self.classifier = torch.nn.Linear(768, 5)
+
+    def forward(self, input_ids, attention_mask, token_type_ids):
+        output_1 = self.l1(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+        hidden_state = output_1[0]
+        pooler = hidden_state[:, 0]
+        pooler = self.pre_classifier(pooler)
+        pooler = torch.nn.ReLU()(pooler)
+        pooler = self.dropout(pooler)
+        output = self.classifier(pooler)
+        return output
+
+model = RobertaClass()
+model.to(device)
+
+#Finetuning Roberta model
+
+# Creating the loss function and optimizer
+loss_function = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(params =  model.parameters(), lr=LEARNING_RATE)
+
+def calcuate_accuracy(preds, targets):
+    n_correct = (preds==targets).sum().item()
+    return n_correct
+
+# Defining the training function on the 80% of the dataset
+
+def train(epoch):
+    tr_loss = 0
+    n_correct = 0
+    nb_tr_steps = 0
+    nb_tr_examples = 0
+    model.train()
+    for _,data in tqdm(enumerate(training_loader, 0)):
+        ids = data['ids'].to(device, dtype = torch.long)
+        mask = data['mask'].to(device, dtype = torch.long)
+        token_type_ids = data['token_type_ids'].to(device, dtype = torch.long)
+        targets = data['targets'].to(device, dtype = torch.long)
+
+        outputs = model(ids, mask, token_type_ids)
+        loss = loss_function(outputs, targets)
+        tr_loss += loss.item()
+        big_val, big_idx = torch.max(outputs.data, dim=1)
+        n_correct += calcuate_accuracy(big_idx, targets)
+
+        nb_tr_steps += 1
+        nb_tr_examples+=targets.size(0)
+        
+        if _%20==0:
+            loss_step2 = tr_loss/nb_tr_steps
+            accu_step2 = (n_correct*100)/nb_tr_examples 
+            print(f"Training Loss per 20 steps: {loss_step2}")
+            print(f"Training Accuracy per 20 steps: {accu_step2}")
+        
+        if _%5000==0:
+            loss_step = tr_loss/nb_tr_steps
+            accu_step = (n_correct*100)/nb_tr_examples 
+            print(f"Training Loss per 5000 steps: {loss_step}")
+            print(f"Training Accuracy per 5000 steps: {accu_step}")
+
+        optimizer.zero_grad()
+        loss.backward()
+        # # When using GPU
+        optimizer.step()
+
+    print(f'The Total Accuracy for Epoch {epoch}: {(n_correct*100)/nb_tr_examples}')
+    epoch_loss = tr_loss/nb_tr_steps
+    epoch_accu = (n_correct*100)/nb_tr_examples
+    print(f"Training Loss Epoch: {epoch_loss}")
+    print(f"Training Accuracy Epoch: {epoch_accu}")
+
+    return
+
+EPOCHS = 3
+for epoch in range(EPOCHS):
+    train(epoch)
+
+#Testing the trained model
+
+def valid(model, testing_loader):
+    model.eval()
+    n_correct = 0; n_wrong = 0; total = 0; tr_loss=0; nb_tr_steps=0; nb_tr_examples=0
+    with torch.no_grad():
+        for _, data in tqdm(enumerate(testing_loader, 0)):
+            ids = data['ids'].to(device, dtype = torch.long)
+            mask = data['mask'].to(device, dtype = torch.long)
+            token_type_ids = data['token_type_ids'].to(device, dtype=torch.long)
+            targets = data['targets'].to(device, dtype = torch.long)
+            outputs = model(ids, mask, token_type_ids).squeeze()
+            loss = loss_function(outputs, targets)
+            tr_loss += loss.item()
+            big_val, big_idx = torch.max(outputs.data, dim=1)
+            n_correct += calcuate_accuracy(big_idx, targets)
+
+            nb_tr_steps += 1
+            nb_tr_examples+=targets.size(0)
+            
+            if _%5000==0:
+                loss_step = tr_loss/nb_tr_steps
+                accu_step = (n_correct*100)/nb_tr_examples
+                print(f"Validation Loss per 100 steps: {loss_step}")
+                print(f"Validation Accuracy per 100 steps: {accu_step}")
+    epoch_loss = tr_loss/nb_tr_steps
+    epoch_accu = (n_correct*100)/nb_tr_examples
+    print(f"Validation Loss Epoch: {epoch_loss}")
+    print(f"Validation Accuracy Epoch: {epoch_accu}")
+    
+    return epoch_accu
+
+acc = valid(model, testing_loader)
+print("Accuracy on test data = %0.2f%%" % acc)
+
